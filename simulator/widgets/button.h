2276e67464aeea789536b4227af83a6a1ffbe353
unknown <santiagoesdras501@gmail.com>
2025-05-11 16:49:14 -0600

    migrating widget creation to poo

--- simulator/widgets/button.h
+++ simulator/widgets/button.h
@@ -1,9 +1,26 @@
 #pragma once
+#include  <iostream>
 #include "raylib.h"
 
-inline bool Button(Rectangle bounds, const char* label) {
-    DrawRectangleRec(bounds, LIGHTGRAY);
-    DrawRectangleLinesEx(bounds, 2, DARKGRAY);
-    DrawText(label, bounds.x + 10, bounds.y + 10, 20, BLACK);
-    return CheckCollisionPointRec(GetMousePosition(), bounds) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON);
-}
+enum class ButtonShape{
+    Rectangle,
+    Rounded,
+    Circle,
+    Triangle,
+    Polygon
+};
+
+class Button{
+    private:
+        std::string buttonName = "";
+        std::string labelText = "";
+        int width = 0;
+        int heigth = 0;
+        int coordX = 0;
+        int coordY = 0;
+        Color shapeColor {};
+        ButtonShape shape = ButtonShape::Rounded;
+    public:
+        Button(std::string btnName, std::string txtLabel, int w, int h, Color color, ButtonShape s, int x, int y): 
+            buttonName(btnName), labelText(txtLabel), width(w), heigth(h), coordX(x - (width/2)), coordY(y - (heigth/2)), shapeColor(color), shape(s){}
+};
